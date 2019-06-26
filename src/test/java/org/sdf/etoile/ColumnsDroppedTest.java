9a178818744b49a6ffafd68d15eaeb6704a6fc69
andreoss <andreoss@sdf.org>
2019-06-26 13:49:26 +0300

    Extract class

--- src/test/java/org/sdf/etoile/ColumnsDroppedTest.java
+++ src/test/java/org/sdf/etoile/ColumnsDroppedTest.java
@@ -1,42 +1,14 @@
 package org.sdf.etoile;
 
-import lombok.RequiredArgsConstructor;
 import org.apache.spark.sql.AnalysisException;
-import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.StructType;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-@RequiredArgsConstructor
-final class FakeInput implements Transformation<Row> {
-    private final SparkSession session;
-    private final StructType ddl;
-    private final List<Row> rows;
-
-    private FakeInput(final SparkSession session, final StructType ddl) {
-        this(session, ddl, Collections.emptyList());
-    }
-
-    FakeInput(final SparkSession session, final String ddl) {
-        this(session, StructType.fromDDL(ddl));
-    }
-
-    @Override
-    public Dataset<Row> get() {
-        return session.createDataFrame(
-                rows,
-                ddl
-        );
-    }
-}
 
 public final class ColumnsDroppedTest {
     private SparkSession session;
