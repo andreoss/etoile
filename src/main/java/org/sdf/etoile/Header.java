c291f950bf5a39548c4356a5fcba8538bdb92ade
andreoss <andreoss@sdf.org>
2019-07-05 20:36:38 +0300

    Fix type parameter

--- src/main/java/org/sdf/etoile/Header.java
+++ src/main/java/org/sdf/etoile/Header.java
@@ -5,11 +5,11 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.catalyst.expressions.GenericRow;
 
 public final class Header<Y> extends Transformation.Envelope<Row> {
-    public Header(final Transformation<Row> df) {
+    public Header(final Transformation<Y> df) {
         this(df.get());
     }
 
-    public Header(final Dataset<Row> df) {
+    public Header(final Dataset<Y> df) {
         super(() -> {
             final Schema schema = new SchemaOf<>(new Stringified<>(() -> df));
             return new Rows(
