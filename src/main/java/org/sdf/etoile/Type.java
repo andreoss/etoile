8a567ead2471d4c5f650736f2e0faa7c85f77eb5
andreoss <andreoss@sdf.org>
2019-07-19 13:00:28 +0300

    issue-111 Add wrapper type for DataType

---
+++ src/main/java/org/sdf/etoile/Type.java
@@ -0,0 +1,45 @@
+package org.sdf.etoile;
+
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.apache.spark.sql.catalyst.parser.CatalystSqlParser$;
+import org.apache.spark.sql.types.DataType;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+interface Type extends Serializable {
+
+    DataType value();
+
+    default String sql() {
+        return value().catalogString();
+    }
+
+    default Class<?> klass() {
+        if (sql().equals("string")) {
+            return String.class;
+        }
+        if (sql().startsWith("timestamp")) {
+            return Timestamp.class;
+        }
+        throw new UnsupportedOperationException();
+    }
+
+    @EqualsAndHashCode
+    @ToString
+    @RequiredArgsConstructor
+    final class Of implements Type {
+        private final DataType dataType;
+
+        public Of(final String type) {
+            this(CatalystSqlParser$.MODULE$.parseDataType(type));
+        }
+
+        @Override
+        public DataType value() {
+            return this.dataType;
+        }
+    }
+}
