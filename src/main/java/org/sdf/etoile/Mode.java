b2eedba132a1c7a9218fe39fd98aeeb1ba38b75b
andreoss <andreoss@sdf.org>
2019-08-09 06:03:17 +0000

    Use EnumUtil for parsing param

--- src/main/java/org/sdf/etoile/Mode.java
+++ src/main/java/org/sdf/etoile/Mode.java
@@ -3,28 +3,17 @@ package org.sdf.etoile;
 import lombok.RequiredArgsConstructor;
 import org.apache.spark.sql.DataFrameWriter;
 import org.apache.spark.sql.SaveMode;
-
-import java.util.Arrays;
-import java.util.function.Predicate;
+import org.apache.spark.util.EnumUtil;
 
 @RequiredArgsConstructor
 final class Mode<X> implements Output<X> {
-    private final String mode;
+    private final String type;
     private final Output<X> orig;
 
     @Override
     public DataFrameWriter<X> get() {
         return this.orig.get()
-                .mode(mode());
+                .mode(EnumUtil.parseIgnoreCase(SaveMode.class, this.type));
     }
 
-    private SaveMode mode() {
-        final Predicate<SaveMode> pred = x -> x.toString()
-                .toLowerCase()
-                .startsWith(mode.toLowerCase());
-        return Arrays.stream(SaveMode.values())
-                .filter(pred)
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(mode));
-    }
 }
