14d5608d2e3aa919db1ed9f2b051505e5030f221
andreoss <andreoss@sdf.org>
2019-08-09 09:39:28 +0000

    Remove dead code

--- src/main/java/org/sdf/etoile/Union.java
+++ src/main/java/org/sdf/etoile/Union.java
@@ -4,10 +4,6 @@ import org.apache.spark.sql.Dataset;
 
 public final class Union<Y> extends Transformation.Envelope<Y> {
 
-    public Union(final Transformation<Y> left, final Transformation<Y> right) {
-        this(left.get(), right.get());
-    }
-
     public Union(final Dataset<Y> left, final Dataset<Y> right) {
         super(() -> left.union(right));
     }
