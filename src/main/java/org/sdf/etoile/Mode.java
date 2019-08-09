a46d875a4d238b5dcdebfe901814951199c9ac62
andreoss <andreoss@sdf.org>
2019-08-09 07:17:19 +0000

    Add ctor

--- src/main/java/org/sdf/etoile/Mode.java
+++ src/main/java/org/sdf/etoile/Mode.java
@@ -7,13 +7,15 @@ import org.apache.spark.util.EnumUtil;
 
 @RequiredArgsConstructor
 final class Mode<X> implements Output<X> {
-    private final String type;
+    private final SaveMode type;
     private final Output<X> orig;
+
+    Mode(final String type, final Output<X> orig) {
+        this(EnumUtil.parseIgnoreCase(SaveMode.class, type), orig);
+    }
 
     @Override
     public DataFrameWriter<X> get() {
-        return this.orig.get()
-                .mode(EnumUtil.parseIgnoreCase(SaveMode.class, this.type));
+        return this.orig.get().mode(this.type);
     }
-
 }
