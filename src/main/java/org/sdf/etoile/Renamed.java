a3878b57c8145cc0b12af687c596f04c39d7809c
andreoss <andreoss@sdf.org>
2019-12-04 12:33:19 +0300

    Add Renamed
    Rename columns according to aliases

---
+++ src/main/java/org/sdf/etoile/Renamed.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright(C) 2019. See COPYING for more.
+ */
+package org.sdf.etoile;
+
+import java.util.Arrays;
+import java.util.Collection;
+import lombok.RequiredArgsConstructor;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.cactoos.collection.Mapped;
+
+/**
+ * Renamed according to aliases.
+ *
+ * @since 0.4.0
+ */
+@RequiredArgsConstructor
+public final class Renamed implements Transformation<Row> {
+    /**
+     * The original tranformation.
+     */
+    private final Transformation<Row> original;
+
+    /**
+     * Column aliases.
+     */
+    private final Collection<Alias> aliases;
+
+    /**
+     * Ctor.
+     * @param aliases Alias.
+     * @param original The original transformation.
+     */
+    public Renamed(final Collection<String> aliases, final Transformation<Row> original) {
+        this(original, new Mapped<>(ColumnAlias::new, aliases));
+    }
+
+    /**
+     * Ctor.
+     * @param original The original.
+     * @param aliases Aliases as strings.
+     */
+    public Renamed(final Transformation<Row> original, final String... aliases) {
+        this(Arrays.asList(aliases), original);
+    }
+
+    @Override
+    public Dataset<Row> get() {
+        Dataset<Row> memo = this.original.get();
+        for (final Alias alias : this.aliases) {
+            memo = memo.withColumnRenamed(alias.reference(), alias.alias());
+        }
+        return memo;
+    }
+}
---
+++ src/test/java/org/sdf/etoile/RenamedTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright(C) 2019. See COPYING for more.
+ */
+package org.sdf.etoile;
+
+import java.io.IOException;
+import java.net.URI;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for {@link Renamed}.
+ * @since 0.4.0
+ */
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
+final class RenamedTest extends SparkTestTemplate {
+    @Test
+    void shouldDoesNothingWhenNoAliases() throws IOException {
+        final URI output = this.temp.newFolder().toPath().resolve("csv").toUri();
+        MatcherAssert.assertThat(
+            "should write unchanged dataset",
+            new CsvText(
+                new Saved<>(
+                    output,
+                    new HeaderCsvOutput<>(
+                        new Renamed(
+                            new FakeInput(this.session, "number int, name string")
+                        )
+                    )
+                )
+            ),
+            new LinesAre("number,name")
+        );
+    }
+
+    @Test
+    void renamesColumns() throws IOException {
+        final URI output = this.temp.newFolder().toPath().resolve("csv").toUri();
+        MatcherAssert.assertThat(
+            "should rename columns to provided aliases",
+            new CsvText(
+                new Saved<>(
+                    output,
+                    new HeaderCsvOutput<>(
+                        new Renamed(
+                            new FakeInput(
+                                this.session,
+                                "number int, name string"
+                            ),
+                            "name as nombre",
+                            "number as numbero"
+                        )
+                    )
+                )
+            ),
+            new LinesAre(
+                "numbero,nombre"
+            )
+        );
+    }
+}
+
